fba3e4e3c62828ca738679b350a04fdb2788c332
Mg Kaung <mymemory692018@gmail.com>
2020-12-09 01:11:53 +0630

    ----

--- doctor/src/main/java/mk/monthlytut/doctor/activities/MainActivity.kt
+++ doctor/src/main/java/mk/monthlytut/doctor/activities/MainActivity.kt
@@ -3,7 +3,9 @@ package mk.monthlytut.doctor.activities
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 import mk.monthlytut.doctor.R
+import mk.monthlytut.doctor.utils.SessionManager
 import mk.padc.share.activities.BaseActivity
 
 class MainActivity : BaseActivity() {
@@ -16,6 +18,6 @@ class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        drname.text= SessionManager.doctor_name
     }
 }
