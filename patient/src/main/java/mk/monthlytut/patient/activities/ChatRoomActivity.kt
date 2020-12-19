0cb1793852498cdc20ad00436b5477e2c2970bd4
Mg Kaung <mymemory692018@gmail.com>
2020-12-19 23:18:09 +0630

    ---

--- patient/src/main/java/mk/monthlytut/patient/activities/ChatRoomActivity.kt
+++ patient/src/main/java/mk/monthlytut/patient/activities/ChatRoomActivity.kt
@@ -95,7 +95,7 @@ class ChatRoomActvity : BaseActivity() , ChatView
            }
 
            if(finish_conservation_status) {
-               mPresenter.onCallPrescription(mConsultationChatVO.id,this)
+
                prescritpionview.visibility = View.VISIBLE
                sendLayoutpatent.visibility =View.GONE
            }else{
